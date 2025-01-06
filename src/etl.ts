57e510aa815f76891455ab5e13e15404422d98c9
Massimiliano Mirra <bard@hyperstruct.net>
2025-01-06 12:08:31 +0100

    chore: Rename log parameter to logger in createOrUpdateDatabase function

--- src/etl.ts
+++ src/etl.ts
@@ -111,6 +111,7 @@ export const extractExtendedServiceInfoWithLlm = async (
   }
 };
 
+// rename the log parameter to logger ai!
 export const createOrUpdateDatabase = async (
   { maxServicesToScrape }: { maxServicesToScrape: number },
   deps: {
