540139993fd6bea24dff16249b361cc19c2b12f1
Massimiliano Mirra <bard@hyperstruct.net>
2025-01-06 12:13:10 +0100

    feat: use logger for script output

--- src/build-database.ts
+++ src/build-database.ts
@@ -1,19 +1,15 @@
+import { pino } from "pino";
 import playwright from "playwright";
 import sqlite from "better-sqlite3";
+import { OpenAI } from "openai";
 import { createOrUpdateDatabase } from "./etl.js";
 
-import { OpenAI } from "openai";
-
+const logger = pino();
 const openai = new OpenAI();
 const browser = await playwright.chromium.connect("ws://127.0.0.1:4000/");
 const db = new sqlite("data.db");
 await createOrUpdateDatabase(
-  {
-    maxServicesToScrape: 3,
-    onProgress(message) {
-      console.log(message);
-    },
-  },
-  { browser, db, openai },
+  { maxServicesToScrape: 3 },
+  { browser, db, openai, logger },
 );
 await browser.close();
